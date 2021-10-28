4f515f3c011de8237c195a72a5bece38b0acb516
Luis David <51376078+luisdavidld551@users.noreply.github.com>
2021-10-27 22:24:51 -0500

    varios registros

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Task;
+use Illuminate\Support\Facades\DB;
+use Exception;
 
 class TaskSeeder extends Seeder
 {
@@ -12,16 +14,62 @@ class TaskSeeder extends Seeder
      *
      * @return void
      */
+    /**
+     * Registros insertados, una lista strings que son el id del registro
+     *
+     * @var array<string>
+     */
+    private $insertados=[];
+    /**
+     * Registros fallidos, una lista de arrays de la forma ['id' => xxx, 'message' => yyy]
+     *
+     * @var array<array{id:string,message:string}>
+     */
+    private $fallidos=[];
+
     public function run()
     {
         $task_data = [
-            'nombre'=> 'Alejandra',
+            ['nombre'=> 'Juan',
             'descripcion' => 'Bien o no',
-            'estado' => 'Mal'
+            'estado' => 'Mal'],
+            ['nombre'=> 'Andres',
+            'descripcion' => 'Bien o no',
+            'estado' => 'Bien'],
+            ['nombre'=> 'Camilo',
+            'descripcion' => 'Bien o no',
+            'estado' => 'Mal'],
+            ['nombre'=> 'Eduardo',
+            'descripcion' => 'Bien o no',
+            'estado' => 'Bien'],
         ];
 
-        $task = new Task($task_data);
-        $task->save();
+        //$task = new Task($task_data);
+        //$task->save();
 
+        foreach ($task_data as $record) {
+            $this->attemptInsertion($record);
+        }
+        dump([
+            'insertados'=>$this->insertados,
+            'fallidos'=>$this->fallidos
+        ]);
+
+    }
+    private function attemptInsertion(array $record):void
+    {
+        try {
+            $task = new Task($record);
+            $task->save();
+
+            //DB::table('tasks')->insert($record);
+            $this->insertados[]=$record['nombre'];
+        } catch (Exception $exception) {
+            $this->fallidos[]=[
+                'id'=>$record['nombre'],
+                'message'=>$exception->getMessage()
+            ];
+            
+        }
     }
 }
